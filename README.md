237f62f69432172dd2082b06d34ed71cb9beabe5
Gabriel Ribeiro <gabrielribss29@gmail.com>
2023-07-03 11:20:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Trybe-Project-03-Pixels-Art
+
+Project developed in the web fundamentals module at trybe
+
+## Description
+
+A web page that contains a functional color palette that can be used to create pixel drawings. For `javascript`, `css` and `html` was used.
+
+## Skills developed
+
+- Manipulate the DOM
+
+- Manipulate the JavaScript
+
+## Preview
+
+![example pixels art](./images/example.png)
---
+++ script.js
@@ -0,0 +1,120 @@
+const cores = document.getElementsByClassName('color');
+let board = 5;
+let pixelBoard = document.getElementById('pixel-board');
+const clean = document.querySelector('#clear-board');
+const pixels = document.getElementsByClassName('pixel');
+const buttonSize = document.getElementById('generate-board');
+const size = document.getElementById('board-size');
+const conteudo = document.getElementById('conteudo');
+const paletaCores = ['black', '#', '#', '#', '#', '#', '#', '#', '#', '#'];
+const changeButton = document.getElementById('change-color');
+const newColor = document.getElementById('color-input');
+
+for (let index = 1; index < paletaCores.length; index += 1) {
+  while (paletaCores[index].length < 7) {
+    paletaCores[index] += Math.floor(Math.random() * 0x10).toString(16);
+  }
+}
+
+for (let index = 0; index < cores.length; index += 1) {
+  const cor = cores[index];
+  cor.id = paletaCores[index];
+  cor.style.backgroundColor = cor.id;
+}
+
+function criaLinha(linha) {
+  for (let index = 0; index < board; index += 1) {
+    const line = document.createElement('div');
+    line.className = 'line';
+    linha.appendChild(line);
+  }
+}
+
+criaLinha(pixelBoard);
+
+let lines = document.querySelectorAll('.line');
+
+function criaPixel(divLine) {
+  for (let index = 0; index < board; index += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    divLine.appendChild(pixel);
+  }
+}
+
+function criaColuna(coluna) {
+  for (let index = 0; index < coluna.length; index += 1) {
+    criaPixel(lines[index]);
+  }
+}
+
+criaColuna(lines);
+
+function color(event) {
+  const selected = document.querySelector('.selected');
+  const tinta = selected.id;
+  const pixel = event.target;
+  pixel.style.backgroundColor = tinta;
+}
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('pixel')) {
+    color(event);
+  }
+}, false);
+
+function changeColor(event) {
+  const selecao = event.target;
+  const corAnterior = document.querySelector('.selected');
+  corAnterior.classList.remove('selected');
+  selecao.classList.add('selected');
+}
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('color')) {
+    changeColor(event);
+  }
+}, false);
+
+function limpar() {
+  for (let index = 0; index < pixels.length; index += 1) {
+    const quadrado = pixels[index];
+    quadrado.style.backgroundColor = 'white';
+  }
+}
+
+clean.addEventListener('click', limpar);
+
+function sizeBoard() {
+  let tamanho = size.value;
+  if (tamanho < 2) {
+    tamanho = 2;
+  } else if (tamanho > 50) {
+    tamanho = 50;
+  }
+  board = tamanho;
+}
+
+size.addEventListener('change', sizeBoard);
+
+function createBoard() {
+  if (size.value === '') {
+    alert('Board inválido!');
+  } else {
+    pixelBoard.innerHTML = null;
+    criaLinha(pixelBoard);
+    lines = document.querySelectorAll('.line');
+    criaColuna(lines);
+  }
+}
+
+function changeNewColor() {
+  const color = newColor.value;
+  const currentColor = document.querySelector('.selected');
+  currentColor.style.backgroundColor = color;
+  currentColor.id = color;
+}
+
+changeButton.addEventListener('click', changeNewColor);
+
+buttonSize.addEventListener('click', createBoard);
